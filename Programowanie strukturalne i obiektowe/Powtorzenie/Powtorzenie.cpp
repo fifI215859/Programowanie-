11ec89f7ec3be56f732c5f97980582ac872c9fc6
GAP <GAP>
2022-09-14 16:25:46 +0200

    fdsfutd

--- Programowanie strukturalne i obiektowe/Powtorzenie/Powtorzenie.cpp
+++ Programowanie strukturalne i obiektowe/Powtorzenie/Powtorzenie.cpp
@@ -1,46 +1,109 @@
 #include <iostream>
-
+/*
 void showMenu()
 {
 	system("cls");
-	std::cout << "Menu:\n";
+	std::cout << "Menu:\n"
 	std::cout << "1. Pole kwadratu\n";
 	std::cout << "2. Pole trojkata\n";
+
+	std::cout << "0, Zamknij progrma\n";
 }
 
-void chooseOptionFromUser()
+int chooseOptionFromUser()
 {
 	std::cout << "Wybierz opcje\n";
 	int selectedOption;
 	std::cin >> selectedOption;
+	return selectedOption;
 }
 
-void doSelectedTask()
+void squareArena()
 {
+	std::cout << "podaja wartos boku kwadratu:\n";
+	int side;
+	std::cin >> side;
+	int area = side * side;
+	std::cout << "PoleKwadratu to " << area << "\n";
+}
+
+void triangleArea()
+{
+	std::cout << "podaja wartos podstawy\n";
+	int base;
+	std::cout << "podaja wartos wysokaœci\n";
+	int height;
+	int area = height * base / 2;
+	std::cout << "Pole Kwadratu to " << area << "\n";
+}
+
+void doSelectedTask(int selectedOption)
+{
+	switch (selectedOption)
+	{
+	case 1:
+		squareArena();
+		break;
+	case 2:
+		triangleArea();
+		break;
+
+
+		case 0;
+		 return;
+	default:
+		std::cout << "Brak opcji w menu:\n"
+			break;
+	}
+
+	system("pause");
+	/*
 	if (selectedOption == 1)
 	{
-		PoleKwadratu();
+		squareArena();
 	}
 	if (selectedOption == 2)
 	{
-		PoleTrojkata();
+		triangleArea();
+	else if (selectedOption)
 	}
-}
-
+}*/
+/*
 void mainProgram()
 {
-	//1. wyœwietlenie menu
-	showMenu();
+	int selected;
+	do
+	{
+		//1. wyœwietlenie menu
+		showMenu();
 
-	//2. wybranie opcji przez u¿ytkownika
-	chooseOptionFromUser();
+		//2. wybranie opcji przez u¿ytkownika
+		int selected = chooseOptionFromUser();
 
-	//3. Wykonanie wybranego zadania
-	doSelectedTask();
+		//3. Wykonanie wybranego zadania
+		doSelectedTask(selected);
+	} while (selected != 0);
 }
 
 void main()
 {
 	mainProgram();
+}*/
+void zad1()
+{
+	int liczba; 
+	std::cout << "podaj liczbe\n";
+	std::cin >> liczba; 
+	for (int i = liczba; i > 0; i--)
+	{
+		if (i % 2 != 0)
+			std::cout << i << ",";
+	}
 }
+void ZAD2()
+{
+	int lenght; 
+	int liczab; 
+	int liczba2;
 
+}
