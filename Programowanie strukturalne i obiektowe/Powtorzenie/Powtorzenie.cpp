40a910e5f30a360d93d6e232c9d7d5c70091ce87
GAP <GAP>
2022-09-19 09:54:42 +0200

    tdiytdy

--- Programowanie strukturalne i obiektowe/Powtorzenie/Powtorzenie.cpp
+++ Programowanie strukturalne i obiektowe/Powtorzenie/Powtorzenie.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-/*
+
 void showMenu()
 {
 	system("cls");
-	std::cout << "Menu:\n"
+	std::cout << "Menu:\n";
 	std::cout << "1. Pole kwadratu\n";
 	std::cout << "2. Pole trojkata\n";
 
@@ -47,17 +47,15 @@ void doSelectedTask(int selectedOption)
 	case 2:
 		triangleArea();
 		break;
-
-
-		case 0;
-		 return;
+	case 3:
+		return;
 	default:
-		std::cout << "Brak opcji w menu:\n"
-			break;
+		std::cout << "Brak opcji w menu:\n";
+		break;
 	}
 
 	system("pause");
-	/*
+
 	if (selectedOption == 1)
 	{
 		squareArena();
@@ -65,45 +63,133 @@ void doSelectedTask(int selectedOption)
 	if (selectedOption == 2)
 	{
 		triangleArea();
-	else if (selectedOption)
-	}
-}*/
-/*
-void mainProgram()
-{
-	int selected;
-	do
+	else
 	{
-		//1. wyœwietlenie menu
-		showMenu();
-
-		//2. wybranie opcji przez u¿ytkownika
-		int selected = chooseOptionFromUser();
-
-		//3. Wykonanie wybranego zadania
-		doSelectedTask(selected);
-	} while (selected != 0);
-}
-
-void main()
-{
-	mainProgram();
-}*/
-void zad1()
-{
-	int liczba; 
-	std::cout << "podaj liczbe\n";
-	std::cin >> liczba; 
-	for (int i = liczba; i > 0; i--)
-	{
-		if (i % 2 != 0)
-			std::cout << i << ",";
+		if (selectedOption)
 	}
-}
-void ZAD2()
-{
-	int lenght; 
-	int liczab; 
-	int liczba2;
+	}
 
-}
+	void mainProgram();
+	{
+		int selected;
+		do
+		{
+			//1. wyœwietlenie menu
+			showMenu();
+
+			//2. wybranie opcji przez u¿ytkownika
+			int selected = chooseOptionFromUser();
+
+			//3. Wykonanie wybranego zadania
+			doSelectedTask(selected);
+		} while (selected != 0);
+	}
+
+	void zad1();
+	{
+		int liczba;
+		std::cout << "podaj liczbe\n";
+		std::cin >> liczba;
+		for (int i = liczba; i > 0; i--)
+		{
+			if (i % 2 != 0)
+				std::cout << i << ",";
+		}
+	}
+	void ZAD2();
+	{
+		int number;
+		int currentFibonaciNumber = 0;
+		int prevOneFibonaciNumber = 0;
+		int prevTwoFibonaciNumber = 1;
+		for (int i = 0; i < 0; i++)
+		{
+			currentFibonaciNumber = prevOneFibonaciNumber + prevTwoFibonaciNumber;
+			std::cout << currentFibonaciNumber;
+
+			prevTwoFibonaciNumber = prevOneFibonaciNumber;
+			prevOneFibonaciNumber = currentFibonaciNumber;
+		}
+	}
+	void zad3();
+	{
+		int liczba;
+		std::cout << "podaj liczbe\n";
+		std::cin >> liczba;
+		for (int i = 1; i <= liczba; i++)
+		{
+			if (liczba % i == 0)
+				std::cout << i << ",";
+		}
+		std::cout << "\n"; \
+			//ver 2.0
+			for (int i; i <= sqrt(liczba); i++)
+			{
+				if (liczba % i == 0)
+					std::cout << "i" << ",";
+				int divider = liczba / i;
+				if (divider != i)
+					std::cout << (liczba / i) << ",";
+			}
+		std::cout << '\n';
+
+
+		//ver 3.0
+		int changeliczba;
+		if (liczba % 2 == 0)
+			changeliczba = 1;
+		else
+			changeliczba = 2;
+		for (int i; i <= sqrt(liczba); i++)
+		{
+			if (liczba % i == 0)
+				std::cout << "i" << ",";
+			int divider = liczba / i;
+			if (divider != i)
+				std::cout << (liczba / i) << ",";
+		}
+		std::cout << '\n';
+	}
+
+	void zad4();
+	{
+		std::cout << "podaj liczbe";
+		int numberFromUser;
+		std::cin >> numberFromUser;
+
+		std::string arrayOfWordsNumber[10] = { "zero", "jeden", "dwa", "trzy", "cztery", "piec", "szesc", "sziedem", "osiem","dziewiec" };
+		int number = numberFromUser;
+		do
+		{
+			int digit = number % 10;
+			number = number / 10;
+
+			std::cout << arrayOfWordsNumber[digit] << "";
+
+		} while (number != 0);
+		std::cout << "\n";
+
+		numberFromUser;
+		std::cin >> numberFromUser;
+
+		number = numberFromUser;
+		std::string stringNumber = "";
+		do
+		{
+			int digit = number % 10;
+			number = number / 10;
+
+			std::cout << arrayOfWordsNumber[digit] << "";
+
+			stringNumber = arrayOfWordsNumber[digit] + " " + stringNumber;
+
+		} while (number != 0);
+		std::cout << stringNumber;
+		std::cout << "\n";
+
+	}
+
+	void main();
+	{
+		mainProgram();
+	}
