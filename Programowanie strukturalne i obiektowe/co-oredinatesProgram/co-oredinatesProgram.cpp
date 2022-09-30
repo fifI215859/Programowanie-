b173d55adc0004ef453f5da454b3ab70ea2a3920
GAP <GAP>
2022-09-30 08:14:18 +0200

    gsrfuf

--- Programowanie strukturalne i obiektowe/co-oredinatesProgram/co-oredinatesProgram.cpp
+++ Programowanie strukturalne i obiektowe/co-oredinatesProgram/co-oredinatesProgram.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+struct Point
+{
+	int x; 
+	int y; 
+};
+
 double calculateDIstance(int a, int b)
 {
 	return sqrt(a * a + b * b);
@@ -8,6 +14,7 @@ double calculateDIstance(int a, int b)
 void CoordinateTestVersion1()
 {
 	int x, y; 
+
 	std::cout << "Podaj x:\n";
 	std::cin >> x; 
 
@@ -20,8 +27,21 @@ void CoordinateTestVersion1()
 	std::cout << "Odleglosc to:" << distance << "\n";
 }
 
+void CoordinateTestVersion2()
+{
+	Point userpoint;
+	std::cout << "Podaj x:\n";
+	std::cin >> userpoint.x;
+
+	std::cout << "Podaj y:\n";
+	std::cin >> userpoint.y;
+
+	//double distance = sqrt(x * x + y * y);
+	double distance = calculateDIstance(userpoint.x, userpoint.y);
+	std::cout << "Odleglosc to:" << distance << "\n";
+}
 int main()
 {
-	CoordinateTestVersion1();
+	CoordinateTestVersion2();
 }
 
